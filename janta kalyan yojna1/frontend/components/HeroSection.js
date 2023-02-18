64f7d53116135da580451da4758c290d1cc4d543
Ashutosh Anand <89116944+dynamicashuu@users.noreply.github.com>
2023-02-19 01:41:06 +0530

    Update HeroSection.js

--- janta kalyan yojna1/frontend/components/HeroSection.js
+++ janta kalyan yojna1/frontend/components/HeroSection.js
@@ -7,9 +7,9 @@ function HeroSection() {
     <div className='grid lg:grid-cols-2 mt-12 gap-8 px-4 lg:px-0 items-center justify-center'>
       <div className='flex mb-12  flex-col justify-center'>
         <h1 className='text-4xl lg:text-6xl font-bold mb-6' >
-          The Loan Stream
+           Crypto for healthcare
         </h1>
-        <p className='mb-6 text-slate-400 max-w-lg lg:text-lg'> Allows DAOs with large treasuries & DeFi users to collateralize their treasuries , and take out loans in the form of streams - without having to sell assets to do so.</p>
+        <p className='mb-6 text-slate-400 max-w-lg lg:text-lg'> </p>
         <div>
         <Link href='/dashboard'>
           <button className='flex gap-2 hover:gap-4 items-center bg-cyan-500 hover:bg-cyan-400 active:bg-cyan-600'>Go to Dashboard
